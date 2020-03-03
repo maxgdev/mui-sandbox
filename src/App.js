810af293ae7f15f781b20bdd3164de1e132279be
maxgdev <max@apphilia.net>
2020-03-03 14:23:59 +0000

    Added AbstractingContainersAndItems

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import Breakpoints from './GridExamples/Breakpoints';
 import BreakpointsAuto from './GridExamples/BreakpointsAuto';
 import FillingSpace from './GridExamples/FillingSpace';
+import AbstractingContainersAndItems from './GridExamples/AbstractingContainersAndItems';
 function App() {
   return (
     <div className="App">
-      <h1>Set Breakpoints</h1>
+      <h3>Set Breakpoints</h3>
       <Breakpoints />
-      <h1>Auto Breakpoints</h1>
+      <h3>Auto Breakpoints</h3>
       <BreakpointsAuto />
-      <h1>Using Justify</h1>
+      <h3>Using Justify</h3>
       <FillingSpace />
+      <h3>Abstracting Containers and Items</h3>
+      <AbstractingContainersAndItems />
     </div>
   );
 }
