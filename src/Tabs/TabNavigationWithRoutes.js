b1243a6232400febb31b38ffe9ec4c884e4dadee
maxgdev <max@apphilia.net>
2020-03-04 22:17:42 +0000

    Revised TabNavigationWithRoutes component

--- src/Tabs/TabNavigationWithRoutes.js
+++ src/Tabs/TabNavigationWithRoutes.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { Fragment } from 'react';
 import { Route, Link } from 'react-router-dom';
 
 import { withStyles } from '@material-ui/core/styles';
@@ -17,55 +17,58 @@ const styles = theme => ({
   }
 });
 
-function TabNavigationWithRoutes({ classes }) {
-  const [value, setValue] = useState(0);
+const TabContainer = ({ value }) => (
+  <AppBar position="static">
+    <Tabs value={value}>
+      <Tab label="Item One" component={Link} to="/" />
+      <Tab label="Item Two" component={Link} to="/page2" />
+      <Tab label="Item Three" component={Link} to="/page3" />
+    </Tabs>
+  </AppBar>
+);
 
-  const onChange = (e, value) => {
-    setValue(value);
-    console.log("tab value: ", value);
-  };
-
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Tabs value={value} onChange={onChange}>
-          <Tab label="Item One" component={Link} to="/" />
-          <Tab label="Item Two" component={Link} to="/page2" />
-          <Tab label="Item Three" component={Link} to="/page3" />
-        </Tabs>
-      </AppBar>
-      <Route
-        exact
-        path="/"
-        render={() => (
+const TabNavigationWithRoutes = withStyles(styles)(({ classes }) => (
+  <div className={classes.root}>
+    <Route
+      exact
+      path="/"
+      render={() => (
+        <Fragment>
+          <TabContainer value={0} />
           <Typography component="div" className={classes.tabContent}>
-            <p>Item One</p>
+          <p>Item One</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae libero diam</p>
           </Typography>
-        )}
-      />
-      <Route
-        exact
-        path="/page2"
-        render={() => (
+        </Fragment>
+      )}
+    />
+    <Route
+      exact
+      path="/page2"
+      render={() => (
+        <Fragment>
+          <TabContainer value={1} />
           <Typography component="div" className={classes.tabContent}>
-            <p>Item Two</p>
+          <p>Item Two</p>
             <p>Donec tellus diam, viverra id mi at, vulputate lobortis nisi. Etiam commodo, odio sit amet imperdiet hendrerit, erat leo consequat lorem, sit amet imperdiet ex tortor in ante</p>
           </Typography>
-        )}
-      />
-      <Route
-        exact
-        path="/page3"
-        render={() => (
+        </Fragment>
+      )}
+    />
+    <Route
+      exact
+      path="/page3"
+      render={() => (
+        <Fragment>
+          <TabContainer value={2} />
           <Typography component="div" className={classes.tabContent}>
-            <p>Item Three</p>
+          <p>Item Three</p>
             <p>In sit amet fringilla nunc. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam accumsan blandit eros et porttitor. Cras vitae tincidunt urna, eget malesuada orci. Integer ornare ac nisl vel faucibus.</p>
           </Typography>
-        )}
-      />
-    </div>
-  );
-}
+        </Fragment>
+      )}
+    />
+  </div>
+));
 
-export default withStyles(styles)(TabNavigationWithRoutes);
+export default TabNavigationWithRoutes;
