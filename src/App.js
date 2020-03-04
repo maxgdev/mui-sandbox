14e400fffd981e14e98845e6b6048a6e84a0c053
maxgdev <max@apphilia.net>
2020-03-04 13:57:39 +0000

    Added DrawerSections component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom'
-import DrawerItemNavigation from './Drawer/DrawerItemNavigation';
+import DrawerSections from './Drawer/DrawerSections';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <DrawerItemNavigation />
-      </Router>
+      <DrawerSections />
     </div>
   );
 }
