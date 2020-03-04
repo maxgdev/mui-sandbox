9a1662f8e9aa9d8215e7044c355d07ddee28f6d7
maxgdev <max@apphilia.net>
2020-03-04 18:48:29 +0000

    Added Tab folder and AppBarIntegration component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import AppBarInteraction from './Drawer/AppBarInteraction';
+import AppBarIntegration from './Tabs/AppBarIntegration';
 
 function App() {
   return (
     <div className="App">
-      <AppBarInteraction />
+      <AppBarIntegration />
     </div>
   );
 }
