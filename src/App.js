229ccc87db6ffdbb5c14dc4c0ef8b466628e4c3f
maxgdev <max@apphilia.net>
2020-03-05 19:56:47 +0000

    Created Styles folder and empty BasicComponentStyle.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import ComponentThemeSettings from './Themes/ComponentThemeSettings'
+import BasicComponentStyles from './Styles/BasicComponentStyles'
 
 function App() {
   return (
     <div className="App">
-      <ComponentThemeSettings />
+      <BasicComponentStyles />
     </div>
   );
 }
---
+++ src/Styles/BasicComponentStyles.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class BasicComponentStyles extends Component {
+    render() {
+        return (
+            <div>
+                BasicComponentStyles
+            </div>
+        )
+    }
+}
