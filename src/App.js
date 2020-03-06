9f3848a8990f80d59686753c4e2036dc8dc91a6d
maxgdev <max@apphilia.net>
2020-03-06 12:25:25 +0000

    Added FilteringRows component and imported into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import SortableColumns from './Tables/SortableColumns'
+import FilteringRows from './Tables/FilteringRows'
 
 function App() {
   return (
     <div className="App">
-      <SortableColumns />
+      <FilteringRows />
     </div>
   );
 }
