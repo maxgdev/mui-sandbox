4aaf6dea0f5140a542b6b1e11f85f702e8283e48
maxgdev <max@apphilia.net>
2020-03-08 16:30:24 +0000

    Added ValidationAndErrorDisplay TextField component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import PlaceholderAndHelperText from './Text/PlaceholderAndHelperText'
+import ValidationAndErrorDisplay from './Text/ValidationAndErrorDisplay'
 
 function App() {
   return (
     <div className="App">
-      <PlaceholderAndHelperText />
+      <ValidationAndErrorDisplay />
     </div>
   );
 }
