5f9bb96b4c821ac9ce80cf2d50e639223b14ed73
maxgdev <max@apphilia.net>
2020-03-08 20:12:23 +0000

    Added IconColorAndState with default, primary, secondary, error, action and disabled color props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
-
+import IconColorAndState from './Icons/IconColorAndState'
 
 function App() {
   return (
     <div className="App">
-      
+      <IconColorAndState />
+      <br />
+      <IconColorAndState color='primary'/>
+      <br />
+      <IconColorAndState color='secondary'/>
+      <br />
+      <IconColorAndState color='error'/>
+      <br />
+      <IconColorAndState color='action'/>
+      <br />
+      <IconColorAndState color='disabled'/>
     </div>
   );
 }
