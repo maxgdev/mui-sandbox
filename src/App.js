5510b9fd538fab8e482573ce662b9e9c270af1c4
maxgdev <max@apphilia.net>
2020-03-08 10:07:03 +0000

    Added LinkButtons component & imported it and react-router-dom into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import ButtonEmphasis from './Buttons/ButtonEmphasis'
+import { BrowserRouter } from 'react-router-dom'
+import LinkButtons from './Buttons/LinkButtons'
 
 function App() {
   return (
     <div className="App">
-      <ButtonEmphasis />
+      <BrowserRouter>
+          <LinkButtons />
+      </BrowserRouter>
     </div>
   );
 }
