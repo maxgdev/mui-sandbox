688e8b0e6bb2be16c26b38c169a42b73f6a89d6b
maxgdev <max@apphilia.net>
2020-03-10 13:46:44 +0000

    Added AbstractingCheckboxGroups component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import AbstractingCheckboxGroups from './Selections/AbstractingCheckboxGroups'
 
 function App() {
   return (
     <div className="App">
-      
+      < AbstractingCheckboxGroups />
     </div>
   );
 }
