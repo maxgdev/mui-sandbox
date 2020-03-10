e4e1a0450887882659229950f068fc231bc31895
maxgdev <max@apphilia.net>
2020-03-10 13:55:10 +0000

    Added adioButtonTypes component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import AbstractingRadioButtonGroups from './Selections/AbstractingRadioButtonGroups'
+import RadioButtonTypes from './Selections/RadioButtonTypes'
 
 function App() {
   return (
     <div className="App">
-      < AbstractingRadioButtonGroups />
+      < RadioButtonTypes />
     </div>
   );
 }
