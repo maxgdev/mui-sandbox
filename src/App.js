e5d3069f36f85e5d5fab41ca2914fd4d6a4d3473
maxgdev <max@apphilia.net>
2020-03-16 10:32:56 +0000

    Added SimpleSelect component example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-
+import SimpleSelect from './Misc/SimpleSelect'
 function App() {
   return (
     <div className="App">
-      
+      < SimpleSelect />
     </div>
   );
 }
